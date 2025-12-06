28eeefc428060b02324aaaf1cc1de526b5b3f98d
Fernanda Gomes <feh.cmrg@outlook.com>
2025-12-06 15:34:26 -0300

    feat(new column): nova coluna para booktype para salvar tipo do livro

---
+++ src/config/db/migrations/1765039893211-NewColumnTypeInBooks.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class NewColumnTypeInBooks1765039893211 implements MigrationInterface {
+    name = 'NewColumnTypeInBooks1765039893211'
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "books" ADD COLUMN "type" varchar(50)`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "books" DROP COLUMN "type"`);
+    }
+
+}
